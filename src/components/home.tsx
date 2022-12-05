a3d55bf143d72e4ed34eabfc635e062b5815dc75
Robin Duqué <robin.duque@outlook.com>
2022-12-05 11:54:36 +0100

    dummy commit to trigger

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -4,7 +4,7 @@ import { Box } from '@chakra-ui/react'
 const Home = (): JSX.Element => {
   return (
     <Box minHeight='50vh' flexGrow={3} mx='2%' boxShadow='base' rounded='lg' bg='white' p='1rem'>
-      <h1>TODO</h1>
+      <h1>TODO TODO</h1>
     </Box>
   )
 }
