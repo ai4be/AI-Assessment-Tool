2e906350a9c63750a5b3f9530709a17daa1b7689
Robin Duqué <robin.duque@outlook.com>
2023-02-06 16:37:50 +0100

    add radar chart with random data

--- src/components/project/checklist/index.tsx
+++ src/components/project/checklist/index.tsx
@@ -4,6 +4,7 @@ import ChecklistTopBar from '@/src/components/project/checklist/top-bar'
 import { Project, Category, Section } from '@/src/types/project'
 import CategoryQuestions from '@/src/components/project/checklist/category-questions'
 import OverviewComponent from '@/src/components/project/checklist/overview'
+import RadarChart from '@/src/components/project/checklist/radar-chart'
 
 interface Props {
   project: Project
@@ -15,7 +16,8 @@ const Checklist: FC<Props> = (props): JSX.Element => {
   return (
     <Box backgroundColor='white' padding='2rem' paddingTop='1rem' borderRadius='1rem'>
       <OverviewComponent />
-      <img src='/radar-chart.jpg' style={{ width: '100%', maxWidth: '800px', margin: 'auto' }} />
+      <RadarChart />
+      {/* <img src='/radar-chart.jpg' style={{ width: '100%', maxWidth: '800px', margin: 'auto' }} /> */}
       <ChecklistTopBar />
       <CategoryQuestions project={props.project} categories={props.categories} marginTop='1rem' />
     </Box>
