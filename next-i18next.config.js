4cca90517ddd3eea79aabb53de65754c9477b658
Priscila Silva <priscila.rodrigues@hotmail.com.br>
2023-03-08 17:23:13 +0100

    Add middleware to set /en as default language, map settings namespace to home so translations for email address verification can appear

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
 
 module.exports = {
-  localePath: path.resolve("./public/locales"),
   i18n: {
     defaultLocale: 'default',
     locales: ['default', 'en', 'fr', 'nl'],
     localeDetection: false,
   },
+  localePath: path.resolve("./public/locales"),
+  trailingSlash: true,
 }
