8d01eea305bba79e4030b48f75c7e99fe279f384
Robin Duqué <robin.duque@outlook.com>
2023-05-10 17:32:40 +0200

    wip moving waves

--- src/components/front-page/content.tsx
+++ src/components/front-page/content.tsx
@@ -7,13 +7,16 @@ import { AltaiSections } from './altai-sections'
 import { TryItOutButton } from './try-out-button'
 import { WaveBlackToGray, WavePurple, WavesOutline } from './waves'
 
+import style from './content.module.css'
+
 export const Content = (): JSX.Element => {
   return (
     <Flex flexDirection='column'>
       <Part1 />
       <WavesOutline />
       <Part2 />
-      <WaveBlackToGray />
+      {/* <WaveBlackToGray /> */}
+      <Waves />
       <Part3 />
       <WaveBlackToGray />
       <TeamMembersContainer />
@@ -46,21 +49,39 @@ const Part1 = (): JSX.Element => {
   )
 }
 
+const Waves = (): JSX.Element => {
+  return (
+    <div>
+      <svg className={style.waves} xmlns='http://www.w3.org/2000/svg' xmlnsXlink='http://www.w3.org/1999/xlink' viewBox='0 24 150 28' preserveAspectRatio='none' shape-rendering='auto'>
+        <defs>
+          <path id='gentle-wave' d='M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z' />
+        </defs>
+        <g className={style.parallax}>
+          <use xlinkHref='#gentle-wave' x='48' y='0' fill='hsla(265, 60%, 18%, 0.95)' />
+          <use xlinkHref='#gentle-wave' x='48' y='3' fill='rgba(255,255,255,0.5)' />
+          <use xlinkHref='#gentle-wave' x='48' y='5' fill='rgba(255,255,255,0.3)' />
+          <use xlinkHref='#gentle-wave' x='48' y='7' fill='#fff' />
+        </g>
+      </svg>
+    </div>
+  )
+}
+
 const Part2 = (): JSX.Element => {
   return (
     <Box py='20vh' px={['5vh', '35vh']} color='white' fontSize='2em' bgColor='rgba(14, 16, 18)'>
-      <Heading fontSize={['2em', '1em']} className='heading-gradient-pink-yellow-white'>Highlight Areas of Risk</Heading>
-      <Text fontSize={['0.75em']} mt='10vh'>In today's fast-paced world, organizations are increasingly adopting AI to streamline operations and improve decision-making. However, AI systems must be developed and <Text as='span' className=''>implemented with caution</Text>, ensuring that they do not compromise fundamental human rights or perpetuate bias and discrimination. Our tool provides a comprehensive assessment of your organization's AI implementation, <Text as='span' className=''>highlighting areas of strength and areas for improvement.</Text></Text>
-      <Heading mt={['20vh', '10vh']} style={{ position: 'relative' }}>
+      <Heading fontSize={['1em']} className='heading-gradient-pink-yellow-white'>Highlight Areas of Risk</Heading>
+      <Text fontSize={['0.75em']} mt='10vh'>In today's fast-paced world, organizations are increasingly adopting AI to streamline operations and improve decision-making. However, AI systems must be developed and <Text as='span' >implemented with caution</Text>, ensuring that they do not compromise fundamental human rights or perpetuate bias and discrimination. Our tool provides a comprehensive assessment of your organization's AI implementation, <Text as='span' >highlighting areas of strength and areas for improvement.</Text></Text>
+      <Heading mt={['10vh']} style={{ position: 'relative' }}>
         <Image src='/frontpage/arrow.png' alt='Demo image' className='fniWYQ' />
-        <Text className='highlight pink' fontSize={['2em', '0.75em']}>Recommendations Report</Text>
+        <Text className='highlight pink' fontSize={['1em']}>Recommendations Report</Text>
       </Heading>
-      <Text fontSize={['2em', '1em']} mt='10vh'>You will also receive detailed <Text as='span' className=''>suggestions and guidance</Text> for improving the trustworthiness of your AI system. This will enable you to build and maintain <Text as='span' className=''>trust</Text> with your customers, employees, and other stakeholders, and <Text as='span' className=''>mitigate the risks</Text> associated with AI implementation.</Text>
-      <Heading mt={['20vh', '10vh']} className='highlight pink' fontSize={['2em', '1em']}>You are in control</Heading>
-      <Text fontSize={['2em', '0.75em']} mt='5vh'>One of the key benefits of our <Text as='span' className=''>open-source</Text> tool is that it can be <Text as='span' className=''>hosted and fully controlled by your organization</Text>. This means that you can maintain complete ownership and control over your data and assessments.</Text>
-      <Text fontSize={['2em', '0.75em']} mt='5vh'>Host the tool on your own servers, you can also ensure that the tool <Text as='span' className=''>meets your</Text> organization's specific <Text as='span' className=''>security and privacy requirements</Text>.</Text>
-      <Text fontSize={['2em', '0.75em']} mt='5vh'>OPEN-SOURCE, modify and adapt the tool to fit your <Text as='span' className=''>organization's unique needs</Text>.</Text>
-      <Text fontSize={['2em', '0.75em']} mt='5vh'>This flexibility and control make our tool an ideal solution for organizations looking to assess the trustworthiness of their AI systems while maintaining full control over their data and assessments.</Text>
+      <Text fontSize={['0.75em']} mt='10vh'>You will also receive detailed <Text as='span'>suggestions and guidance</Text> for improving the trustworthiness of your AI system. This will enable you to build and maintain <Text as='span' >trust</Text> with your customers, employees, and other stakeholders, and <Text as='span' >mitigate the risks</Text> associated with AI implementation.</Text>
+      <Heading mt={['10vh']} className='highlight pink' fontSize={['1em']}>You are in control</Heading>
+      <Text fontSize={['0.75em']} mt='5vh'>One of the key benefits of our <Text as='span'>open-source</Text> tool is that it can be <Text as='span' >hosted and fully controlled by your organization</Text>. This means that you can maintain complete ownership and control over your data and assessments.</Text>
+      <Text fontSize={['0.75em']} mt='5vh'>Host the tool on your own servers, you can also ensure that the tool <Text as='span'>meets your</Text> organization's specific <Text as='span' >security and privacy requirements</Text>.</Text>
+      <Text fontSize={['0.75em']} mt='5vh'><Text as='span' textDecoration='underline' fontWeight='bold'>OPEN-SOURCE</Text>, modify and adapt the tool to fit your <Text as='span' >organization's unique needs</Text>.</Text>
+      <Text fontSize={['0.75em']} mt='5vh'>This flexibility and control make our tool an ideal solution for organizations looking to assess the trustworthiness of their AI systems while maintaining full control over their data and assessments.</Text>
       <Center mt='10vh'>
         <TryItOutButton link={checkEnvironment()} label='Install it here' />
       </Center>
@@ -75,7 +96,7 @@ const Part3 = (): JSX.Element => {
       <Box>
         <Image src='/frontpage/demo2.png' alt='Demo image' height='30vh' width='50vh' />
       </Box>
-      <Flex  justifyContent='space-between' mt='10vh'>
+      <Flex justifyContent='space-between' mt='10vh'>
         <Wrap>
           <Box width='12em'>
             <Text fontSize={['2em', '1em']}>The demo instance is a publicly available instance for trying out the AI Ethics Assessment Tool and can be found at altai.ai4belgium.be.
